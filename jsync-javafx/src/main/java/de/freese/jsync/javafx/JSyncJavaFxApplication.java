5aecc0eabc64eb2978fcb49eab91df6c9df1487e
tfreese <commercial@freese-home.de>
2020-12-01 19:21:04 +0100

    polish

--- jsync-javafx/src/main/java/de/freese/jsync/javafx/JSyncJavaFxApplication.java
+++ jsync-javafx/src/main/java/de/freese/jsync/javafx/JSyncJavaFxApplication.java
@@ -16,7 +16,7 @@ import org.slf4j.LoggerFactory;
  * <br>
  * In Eclipse:<br>
  * <ol>
- * <li>Konstruktor muss public sein
+ * <li>Konstruktor muss public empty-arg sein oder nicht vorhanden sein.
  * <li>VM-Parameter: --add-modules javafx.controls
  * <li>Module-Classpath: OpenJFX die jeweils 2 Jars für javafx-base, javafx-controls und javafx-graphics hinzufügen
  * </ol>
