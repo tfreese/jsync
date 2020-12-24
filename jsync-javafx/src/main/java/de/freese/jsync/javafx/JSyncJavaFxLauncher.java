b4d9965dcd9512f3879ecb9af8ec77e63ff4435c
tfreese <commercial@freese-home.de>
2020-12-24 12:34:21 +0100

    polish

---
+++ jsync-javafx/src/main/java/de/freese/jsync/javafx/JSyncJavaFxLauncher.java
@@ -0,0 +1,16 @@
+// Created: 24.12.2020
+package de.freese.jsync.javafx;
+
+/**
+ * @author Thomas Freese
+ */
+public class JSyncJavaFxLauncher
+{
+    /**
+     * @param args String[]
+     */
+    public static void main(final String[] args)
+    {
+        JSyncJavaFxApplication.main(args);
+    }
+}
