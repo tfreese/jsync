2696e1f81375fabeda847a07c81afdddc4437e84
tfreese <community@freese-home.de>
2023-07-18 18:24:22 +0200

    polish

---
+++ jsync-core/src/main/java/de/freese/jsync/utils/io/MonitoringInputStream.java
@@ -0,0 +1,90 @@
+// Created: 11.01.2017
+package de.freese.jsync.utils.io;
+
+import java.io.FilterInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Objects;
+import java.util.function.BiConsumer;
+import java.util.function.LongConsumer;
+
+import javax.swing.ProgressMonitorInputStream;
+
+/**
+ * @author Thomas Freese
+ * @see ProgressMonitorInputStream
+ */
+public class MonitoringInputStream extends FilterInputStream {
+    private final LongConsumer bytesReadConsumer;
+
+    private final boolean closeDelegate;
+
+    private long bytesRead;
+
+    /**
+     * @param bytesReadConsumer {@link BiConsumer}; 1st Parameter = bytesRead, 2nd Parameter = Size
+     */
+    public MonitoringInputStream(final InputStream delegate, final BiConsumer<Long, Long> bytesReadConsumer, final long size, final boolean closeDelegate) {
+        this(delegate, br -> bytesReadConsumer.accept(br, size), closeDelegate);
+    }
+
+    public MonitoringInputStream(final InputStream delegate, final LongConsumer bytesReadConsumer, final boolean closeDelegate) {
+        super(delegate);
+
+        this.bytesReadConsumer = Objects.requireNonNull(bytesReadConsumer, "bytesReadConsumer required");
+        this.closeDelegate = closeDelegate;
+    }
+
+    @Override
+    public void close() throws IOException {
+        if (this.closeDelegate) {
+            super.close();
+        }
+    }
+
+    @Override
+    public int read() throws IOException {
+        int read = super.read();
+
+        this.bytesRead++;
+
+        this.bytesReadConsumer.accept(this.bytesRead);
+
+        return read;
+    }
+
+    @Override
+    public int read(final byte[] b, final int off, final int len) throws IOException {
+        int readCount = super.read(b, off, len);
+
+        if (readCount > 0) {
+            this.bytesRead += readCount;
+
+            this.bytesReadConsumer.accept(this.bytesRead);
+        }
+
+        return readCount;
+    }
+
+    @Override
+    public synchronized void reset() throws IOException {
+        super.reset();
+
+        this.bytesRead -= available();
+
+        this.bytesReadConsumer.accept(this.bytesRead);
+    }
+
+    @Override
+    public long skip(final long n) throws IOException {
+        long readCount = super.skip(n);
+
+        if (readCount > 0) {
+            this.bytesRead += readCount;
+
+            this.bytesReadConsumer.accept(this.bytesRead);
+        }
+
+        return readCount;
+    }
+}
---
+++ jsync-core/src/main/java/de/freese/jsync/utils/io/MonitoringOutputStream.java
@@ -0,0 +1,59 @@
+// Created: 11.01.2017
+package de.freese.jsync.utils.io;
+
+import java.io.FilterOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.util.Objects;
+import java.util.function.BiConsumer;
+import java.util.function.LongConsumer;
+
+/**
+ * @author Thomas Freese
+ */
+public class MonitoringOutputStream extends FilterOutputStream {
+    private final LongConsumer bytesWrittenConsumer;
+
+    private final boolean closeDelegate;
+
+    private long bytesWritten;
+
+    /**
+     * @param bytesWrittenConsumer {@link BiConsumer}; 1st Parameter = bytesRead, 2nd Parameter = Size
+     */
+    public MonitoringOutputStream(final OutputStream delegate, final BiConsumer<Long, Long> bytesWrittenConsumer, final long size, final boolean closeDelegate) {
+        this(delegate, bw -> bytesWrittenConsumer.accept(bw, size), closeDelegate);
+    }
+
+    public MonitoringOutputStream(final OutputStream delegate, final LongConsumer bytesWrittenConsumer, final boolean closeDelegate) {
+        super(delegate);
+
+        this.bytesWrittenConsumer = Objects.requireNonNull(bytesWrittenConsumer, "bytesWrittenConsumer required");
+        this.closeDelegate = closeDelegate;
+    }
+
+    @Override
+    public void close() throws IOException {
+        if (this.closeDelegate) {
+            super.close();
+        }
+    }
+
+    @Override
+    public void write(final byte[] b, final int off, final int len) throws IOException {
+        super.write(b, off, len);
+
+        this.bytesWritten += len;
+
+        this.bytesWrittenConsumer.accept(this.bytesWritten);
+    }
+
+    @Override
+    public void write(final int b) throws IOException {
+        super.write(b);
+
+        this.bytesWritten++;
+
+        this.bytesWrittenConsumer.accept(this.bytesWritten);
+    }
+}
